f538988b2e9701ea220bc697043a1e697114510e
johnny <johnny@Johnnys-MacBook-Pro.local>
2018-11-23 13:54:48 +0800

    add vagrant vbox ubuntu

---
+++ fullstack-nanodegree-vm-master/vagrant-air/c c++/w2q6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//
+int f (int x) { 
+  int answer = 0        
+  for (int i = 0; i < X; i++) {  
+    answer += i * i;             
+  }                              
+  return answer;
+}
+
+//
+int main(){
+  //
+  int i = f(6);
+  //
+  return 1;
+  //
+}
